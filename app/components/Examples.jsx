361a30437f3729614a33ab2bb4f8ae5a911f1204
nysertxs <ponceluisangel96@gmail.com>
2016-09-05 11:48:34 -0500

    Update copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ var React = require("react");
 // var Examples = (props) => <h3>Examples Page</h3>;
 var Examples = (props) => {
   return (
-    <h3>Examples Page</h3>
+    <h3>Examples Page!</h3>
   );
 };
 
