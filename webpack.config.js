36ec1fcb88c574010ea0cffa1cd2ca3b09d967bf
nysertxs <ponceluisangel96@gmail.com>
2016-09-08 11:31:30 -0500

    switch to scss

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       WeatherMessage: "app/components/WeatherMessage.jsx",
       openWeatherMap: "app/api/openWeatherMap.jsx",
       ErrorModal: "app/components/ErrorModal.jsx",
-      ApplicationStyles: "app/styles/app.css"
+      ApplicationStyles: "app/styles/app.scss"
     },
     extensions: ["", ".js", ".jsx"]
   },
